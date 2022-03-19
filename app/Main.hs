10fdaa7de383a4a08eda5378b9ad31dd9e61e1b8
Dasha Lary <dasha4988@gmail.com>
2022-03-19 16:35:27 -0400

    got the server running

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import Lib
+-- import Lib
+
+import Lucid
+import Web.Spock
+import Web.Spock.Config
+import Web.Spock.Lucid (lucid)
+
+type Server a = SpockM () () () a
+
+app :: Server ()
+app = get root $
+  lucid $ do
+    h1_ "Hello friend :)"
+    p_ "How are you?"
 
 main :: IO ()
-main = putStrLn "My todo list"
+main = do
+  cfg <- defaultSpockCfg () PCNoDatabase ()
+  runSpock 8080 (spock cfg app)
