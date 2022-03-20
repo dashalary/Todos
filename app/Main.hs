ab4a707d82e1fafe13c4547c27f91f8ff6febce3
Dasha Lary <dasha4988@gmail.com>
2022-03-20 18:02:16 -0400

    added form

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ module Main where
 
 -- import Lib
 
+import Data.Semigroup ((<>))
 import Control.Monad (forM_)
 import Control.Monad.IO.Class (liftIO)
 import Data.IORef
@@ -20,15 +21,32 @@ newtype ServerState = ServerState {todos :: IORef [Todo]}
 type Server a = SpockM () () ServerState a
 
 app :: Server ()
-app = get root $ do
-  todos' <- getState >>= (liftIO . readIORef . todos)
-  lucid $ do
-    h1_ "Todo List"
-    ul_ $
-      forM_ todos' $ \todo -> li_ $ do
-        toHtml (author todo)
-        ": "
-        toHtml (contents todo)
+app = 
+  get root $ do
+    todos' <- getState >>= (liftIO . readIORef . todos)
+    lucid $ do
+        h1_ "Todo List"
+        ul_ $
+        forM_ todos' $ \todo -> li_ $ do
+            toHtml (author todo)
+            ": "
+            toHtml (contents todo)
+        h2_ "New Todo"
+        form_ [method_ "post"] $ do
+            label_ $ do
+                "Author: "
+                input_ [name_ "author"]
+            label_ $ do
+                "Contents: "
+                textarea_ [name_ "contents"] ""
+            input_ [type_ "submit", value_ "Add Todo"]
+  post root $ do
+      author <- param' "author"
+      contents <- param' "contents"
+      todosRef <- todos <$> getState
+      liftIO $ atomicModifyIORef' todosRef $ \todos ->
+          (todos <> [Todo author contents], ())
+      redirect "/"
 
 main :: IO ()
 main = do
