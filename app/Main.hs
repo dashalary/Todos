6d53ba2e46fb88b9b491fa9b40e8bc7c2b9177a7
Dasha Lary <dasha4988@gmail.com>
2022-03-20 17:54:53 -0400

    added todos

--- app/Main.hs
+++ app/Main.hs
@@ -4,20 +4,39 @@ module Main where
 
 -- import Lib
 
+import Control.Monad (forM_)
+import Control.Monad.IO.Class (liftIO)
+import Data.IORef
+import Data.Text (Text)
 import Lucid
 import Web.Spock
 import Web.Spock.Config
 import Web.Spock.Lucid (lucid)
 
-type Server a = SpockM () () () a
+data Todo = Todo {author :: Text, contents :: Text}
+
+newtype ServerState = ServerState {todos :: IORef [Todo]}
+
+type Server a = SpockM () () ServerState a
 
 app :: Server ()
-app = get root $
+app = get root $ do
+  todos' <- getState >>= (liftIO . readIORef . todos)
   lucid $ do
-    h1_ "Hello friend :)"
-    p_ "How are you?"
+    h1_ "Todo List"
+    ul_ $
+      forM_ todos' $ \todo -> li_ $ do
+        toHtml (author todo)
+        ": "
+        toHtml (contents todo)
 
 main :: IO ()
 main = do
-  cfg <- defaultSpockCfg () PCNoDatabase ()
+  st <-
+    ServerState
+      <$> newIORef
+        [ Todo "Dasha" "Call Dylan",
+          Todo "Doug" "Work on the car"
+        ]
+  cfg <- defaultSpockCfg () PCNoDatabase st
   runSpock 8080 (spock cfg app)
